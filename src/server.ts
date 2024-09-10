0ef49f08a66e9c771773518257545653616f4e27
Juanse <pcjvan@gmail.com>
2024-09-10 16:31:24 -0500

    add entry as index

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
 import express from "express";
 
-const app = express();
+export const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
-});
-
-app.listen(3000, () => {
-  console.log("server is running using port 3000");
 });
